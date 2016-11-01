780f5e509cf19fd9ce8539e183af756d8317de95
Sean Norton <Norton.Sean.M@gmail.com>
2016-11-01 17:24:57 -0500

    add some basic rails model tests

--- collab-cont/app/models/contract.rb
+++ collab-cont/app/models/contract.rb
@@ -1,3 +1,5 @@
 class Contract < ApplicationRecord
+  validates :title, presence: true
+  validates :link, presence: true
   has_many :decisions
 end
---
+++ collab-cont/test/controllers/decisions_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class DecisionsControllerTest < ActionDispatch::IntegrationTest
+    @contract = Contract.new(title: "hello", link: "12345")
+  test "Should not save decision without description" do
+    decision = Decision.new(contract_id: @contract.id)
+    assert_not decision.save, "Saved decision without description"
+  end
+
+  test "Should not save decision without contract_id" do
+    decision = Decision.new(description: "This is the description")
+    assert_not decision.save, "Saved decision without contract_id"
+  end
+
+  test "Should save decision with description & contract_id" do
+    decision = Decision.new(description: "This is the description", contract_id: 1)
+    assert decision.save, "Did not save decision with description and contract_id"
+  end
+
+end
