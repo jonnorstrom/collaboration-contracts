a8594ab3eb682b606d2fa085d622ffa8a065e5fd
Sean Norton <Norton.Sean.M@gmail.com>
2016-11-17 19:04:29 -0600

    Add privileges to viewing answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,16 +5,25 @@ class Answer < ApplicationRecord
 
   belongs_to :decision
 
-  def self.find_all_names(type, decision_id)
-    names = Decision.find(decision_id).answers.where(answer: type).map { |a| a.name }
+  def self.find_all_names(type, decision_id, user_id, user_name)
+    names = Decision.find(decision_id).answers.where(answer: type).map do |answer|
+      if answer.viewable?(user_id, user_name)
+        return answer.name
+      end
+    end
     return names.join(", ")
   end
 
-  def contract_link
-    Contract.find(self.decision.contract_id).link
+  def contract
+    Contract.find(self.decision.contract_id)
   end
 
   def decision
     Decision.find(self.decision_id)
   end
+
+  def viewable?(user_id, user_name)
+    self.name == user_name || self.contract.owner?(user_id)
+  end
+
 end
