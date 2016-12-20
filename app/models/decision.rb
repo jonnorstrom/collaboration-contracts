74337f3539b5d63c26a50c086bb309761496abbc
Sean Norton <Norton.Sean.M@gmail.com>
2016-12-20 17:05:00 -0600

    finalize decision model tests

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -5,8 +5,9 @@ class Decision < ApplicationRecord
   has_many :answers, :dependent => :destroy
   belongs_to :contract
 
-  def user_answer_type?(type, user)
-      self.answers.exists?(answer: type, name: user)
+  # this is used to pre-check radio_button on answer modal
+  def user_answer_type?(type, user_name)
+      self.answers.exists?(answer: type, name: user_name)
   end
 
 end
--- spec/models/decision_spec.rb
+++ spec/models/decision_spec.rb
@@ -1,25 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Decision, type: :model do
-  subject {
-    FactoryGirl.create(:decision_with_answers)
-  }
 
-  it {should have_many(:answers)}
-  it {should belong_to(:contract)}
+  describe "validations" do
+    before do
+      @decision = build(:decision_with_answers)
+    end
 
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
+    it {should have_many(:answers).dependent(:destroy)}
+    it {should belong_to(:contract)}
+
+    it "is valid with valid attributes" do
+      expect(@decision).to be_valid
+    end
+    it "is not valid without description" do
+      @decision.description = nil
+      expect(@decision).to_not be_valid
+    end
+    it "is not valid without contract_id" do
+      @decision.contract_id = nil
+      expect(@decision).to_not be_valid
+    end
   end
 
-  it "is not valid without description" do
-    subject.description = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without contract_id" do
-    subject.contract_id = nil
-    expect(subject).to_not be_valid
+  describe "user_answer_type?" do
+    before do
+      @decision = create(:decision)
+      @answer = Answer.create(decision_id: @decision.id, answer: "Explain", name: "Joe Shmo")
+    end
+    
+    it "returns true if decision has answer with answer.type and user.name" do
+      expect(@decision.user_answer_type?(@answer.answer, @answer.name)).to be_truthy
+    end
+    it "returns false if user.name is wrong" do
+      expect(@decision.user_answer_type?(@answer.answer, "BadBadNotGood")).to be_falsy
+    end
+    it "returns false if answer.type is wrong" do
+      expect(@decision.user_answer_type?("BadBadNotgood", @answer.name)).to be_falsy
+    end
   end
 
 end
