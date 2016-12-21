72b665d5745f1582ceb3da58a94e7eb0a61c34cf
Sean Norton <Norton.Sean.M@gmail.com>
2016-12-20 21:54:23 -0600

    test for contracts controller up to create

--- spec/controllers/contract_controller_spec.rb
+++ spec/controllers/contract_controller_spec.rb
@@ -14,4 +14,51 @@ RSpec.describe ContractsController, type: :controller do
     end
   end
 
+  describe "Create" do
+
+    context "with user" do
+      before do
+        session[:user_id] = 1
+        process :create, method: :post, params: {contract: {title: "Contract Title", theme: "Contract Theme"} }
+      end
+      it "assigns @contract" do
+        expect(assigns(:contract)).to be_a(Contract)
+      end
+      it "should set contract user when logged in" do
+        expect(assigns(:contract).user_id).to eq(1)
+      end
+      it "should not create a new user" do
+        expect(assigns(:user)).to be(nil)
+      end
+    end
+
+    context "without user" do
+      before do
+        process :create, method: :post, params: {contract: {title: "Contract Title", theme: "Contract Theme"} }
+      end
+      it "should create user when not signed in" do
+        expect(assigns(:user)).to be_a(User)
+      end
+      it "should store user_id in session" do
+        expect(session[:user_id]).to be_truthy
+      end
+    end
+
+    context "with||without user" do
+      it "should redirect to contracts page when saved" do
+        process :create, method: :post, params: {contract: {title: "Contract Title", theme: "Contract Theme"} }
+        assert_response :redirect
+      end
+      before do
+        process :create, method: :post, params: {contract: {title: "", theme: "Contract Theme"} }
+      end
+      it "should render home/index when contract cannot save" do
+        expect(response).to render_template("home/index")
+      end
+      it "should assign error_messages" do
+        expect(assigns(:error_messages)).to eq(["Title can't be blank"])
+      end
+    end
+  end
+
 end
