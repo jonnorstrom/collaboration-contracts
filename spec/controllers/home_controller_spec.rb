298d2c691c3c22d029b26e23d5cebffca77f7f33
Sean Norton <Norton.Sean.M@gmail.com>
2016-12-22 16:29:30 -0600

    add contract controller specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET index" do
-    it "has a 200 status code" do
+    before do
       get :index
+    end
+    it "has a 200 status code" do
       expect(response.status).to eq(200)
+    end
+    it "renders home/index" do
+      expect(response).to render_template("home/index")
     end
   end
 end
